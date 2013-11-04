777195566be8bb169b12c4779bbc8ca5fa60b00b
TianyuXie <xiety0001@gmail.com>
2013-11-04 18:55:49 +0800

    live recorder activity

--- src/com/pplive/liveplatform/ui/LiveRecorderActivity.java
+++ src/com/pplive/liveplatform/ui/LiveRecorderActivity.java
@@ -2,6 +2,7 @@ package com.pplive.liveplatform.ui;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.WindowManager;
 
 import com.pplive.liveplatform.R;
 
@@ -10,7 +11,12 @@ public class LiveRecorderActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+        
         setContentView(R.layout.activity_live_recorder);
+        
+        
     }
 
 }
---
+++ src/com/pplive/liveplatform/ui/recorder/Preview.java
@@ -0,0 +1,17 @@
+package com.pplive.liveplatform.ui.recorder;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.SurfaceView;
+
+public class Preview extends SurfaceView {
+    
+    public Preview(Context context, AttributeSet attrs) {
+        this(context, attrs, 0 /* defStyle */);
+    }
+
+    public Preview(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+    }
+
+}
