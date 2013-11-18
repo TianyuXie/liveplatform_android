bfe2283f6da0d69bc161b857291c99af226e7c67
谢天宇 LeoXie <leoxie@pptv.com>
2013-11-18 13:44:29 +0800

    Added: AsyncImageView.java

--- src/com/pplive/liveplatform/ui/record/LiveMediaRecoder.java
+++ src/com/pplive/liveplatform/ui/record/LiveMediaRecoder.java
@@ -12,10 +12,12 @@ public class LiveMediaRecoder {
         PPboxSink.init(ctx.getApplicationContext());
         mCapture = new PPboxSink(camera);
 
-        //String url = "rtmp://192.168.27.253/live/android?format=rtm&mux.Muxer.video_codec=AVC1&mux.Muxer.audio_codec=MP4A&mux.Encoder.AVC1.param={preset:veryfast,tune:zerolatency,profile:baseline}";
-        String url = "rtmp://192.168.27.253/live/android";
-
+        //        String url = "rtmp://183.129.205.101:1936/push/mobi1";
+        //        String url = "rtmp://192.168.27.253/live/android";
         //        String url = "/sdcard/pplog/a.flv";
+
+        String url = "rtmp://10.0.0.200/live/android";
+
         mCapture.open(url);
     }
 
---
+++ src/com/pplive/liveplatform/ui/widget/AsyncImageView.java
@@ -0,0 +1,38 @@
+package com.pplive.liveplatform.ui.widget;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.ImageView;
+
+import com.nostra13.universalimageloader.core.DisplayImageOptions;
+import com.nostra13.universalimageloader.core.ImageLoader;
+import com.nostra13.universalimageloader.core.assist.ImageLoadingListener;
+
+public class AsyncImageView extends ImageView {
+
+    private ImageLoader mImageLoader = ImageLoader.getInstance();
+
+    public AsyncImageView(Context context, AttributeSet attrs) {
+        this(context, attrs, 0 /* defStyle */);
+    }
+
+    public AsyncImageView(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+    }
+
+    public void setImageAsync(String imageUri) {
+        mImageLoader.displayImage(imageUri, this);
+    }
+
+    public void setImageAsync(String imageUri, DisplayImageOptions options) {
+        mImageLoader.displayImage(imageUri, this, options);
+    }
+
+    public void setImageAsync(String imageUri, ImageLoadingListener listener) {
+        mImageLoader.displayImage(imageUri, this, listener);
+    }
+    
+    public void setImageAsync(String imageUri, DisplayImageOptions options, ImageLoadingListener listener) {
+        mImageLoader.displayImage(imageUri, this, options, listener);
+    }
+}
