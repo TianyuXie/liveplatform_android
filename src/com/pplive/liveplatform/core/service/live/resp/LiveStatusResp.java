14c59e911ed183c38ed915b8f33f13cf460d9c09
谢天宇 LeoXie <leoxie@pptv.com>
2013-12-25 14:01:55 +0800

    Add getLiveStatus

---
+++ src/com/pplive/liveplatform/core/service/live/resp/LiveStatusResp.java
@@ -0,0 +1,7 @@
+package com.pplive.liveplatform.core.service.live.resp;
+
+import com.pplive.liveplatform.core.service.live.model.LiveStatus;
+
+public class LiveStatusResp extends Resp<LiveStatus>{
+
+}
