8fef933c142e89213467c21147bede166609c167
xianjiazhang <xianjiazhang@pptv.com>
2014-01-05 11:47:56 +0800

    update

--- src/com/pplive/liveplatform/ui/HomeActivity.java
+++ src/com/pplive/liveplatform/ui/HomeActivity.java
@@ -30,6 +30,7 @@ import com.pplive.liveplatform.ui.widget.AnimDoor;
 import com.pplive.liveplatform.ui.widget.LoadingButton;
 import com.pplive.liveplatform.ui.widget.SideBar;
 import com.pplive.liveplatform.ui.widget.slide.SlidableContainer;
+import com.pplive.liveplatform.update.Update;
 import com.pplive.liveplatform.util.DisplayUtil;
 
 public class HomeActivity extends FragmentActivity implements HomeFragment.Callback, SlidableContainer.OnSlideListener {
@@ -91,6 +92,7 @@ public class HomeActivity extends FragmentActivity implements HomeFragment.Callb
         mStatusUpAnimation.setFillAfter(true);
         mStatusUpAnimation.setDuration(TIME_BUTTON_UP);
         mStatusUpAnimation.setAnimationListener(upAnimationListener);
+        Update.doUpdateAPP(this);
 
     }
 
