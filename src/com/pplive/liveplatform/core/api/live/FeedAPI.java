40f39f451121370b88cd26ce2497f562128f1528
leoxie <leoxie@pptv.com>
2014-07-15 20:52:10 +0800

    Feed del

--- src/com/pplive/liveplatform/core/api/live/FeedAPI.java
+++ src/com/pplive/liveplatform/core/api/live/FeedAPI.java
@@ -12,6 +12,7 @@ import com.pplive.liveplatform.core.api.live.auth.UserTokenAuthentication;
 import com.pplive.liveplatform.core.api.live.model.FallList;
 import com.pplive.liveplatform.core.api.live.model.Feed;
 import com.pplive.liveplatform.core.api.live.resp.FeedFallListResp;
+import com.pplive.liveplatform.core.api.live.resp.MessageResp;
 import com.pplive.liveplatform.util.URL.Protocol;
 
 public class FeedAPI extends RESTfulAPI {
@@ -23,6 +24,9 @@ public class FeedAPI extends RESTfulAPI {
 
     private static final String TEMPLATE_GET_SYS_MSG = new BaseURL(Protocol.HTTP, Constants.LIVEPLATFORM_API_TEST_HOST,
             "/ft/feed/v3/pptv/user/{username}/sysmsgs?nexttk={nexttk}&fallcount={fallcount}").toString();
+
+    private static final String TEMPLATE_DELETE_FEED = new BaseURL(Protocol.HTTP, Constants.LIVEPLATFORM_API_TEST_HOST,
+            "/ft/feed/v3/pptv/user/{username}/{snstype}/{feedtype}").toString();
 
     private static final FeedAPI sInstance = new FeedAPI();
 
@@ -72,6 +76,32 @@ public class FeedAPI extends RESTfulAPI {
 
             if (0 == resp.getError()) {
                 return resp.getData();
+            }
+        } catch (Exception e) {
+            Log.w(TAG, e.toString());
+        }
+
+        if (null != resp) {
+            throw new LiveHttpException(resp.getError());
+        } else {
+            throw new LiveHttpException();
+        }
+    }
+
+    public boolean deleteFeed(String coToken, String username, Feed feed) throws LiveHttpException {
+        UserTokenAuthentication coTokenAuthentication = new UserTokenAuthentication(coToken);
+        mHttpHeaders.setAuthorization(coTokenAuthentication);
+        HttpEntity<String> req = new HttpEntity<String>(mHttpHeaders);
+
+        MessageResp resp = null;
+        try {
+            HttpEntity<MessageResp> rep = mRestTemplate.exchange(TEMPLATE_DELETE_FEED, HttpMethod.DELETE, req, MessageResp.class, username, feed.getSnsType(),
+                    feed.getFeedType());
+
+            resp = rep.getBody();
+
+            if (0 == resp.getError()) {
+                return true;
             }
         } catch (Exception e) {
             Log.w(TAG, e.toString());
